2b5f874b0bf31e7f9f4e523b333220fd86d1af4c
Tigran Aginyan <taginyan91@gmail.comm>
2022-08-20 02:39:29 +0400

    Improvement animation using GREENSOCK library

--- script.js
+++ script.js
@@ -2,85 +2,90 @@ const LEFT_IAMGE = document.getElementById("left-image");
 const FRAME1 = document.getElementById("frame1-image");
 const CENTER_IMAGE = document.getElementById("center-image");
 const FRAME2 = document.getElementById("frame2-image");
-const RIGHT_IMAGE1 = document.getElementById("first");
-const RIGHT_IMAGE2 = document.getElementById("second");
-const RIGHT_IMAGE3 = document.getElementById("third");
+const TODO1 = document.getElementById("todo1");
+const TODO2 = document.getElementById("todo2");
+const TODO3 = document.getElementById("todo3");
+const TODO4 = document.getElementById("todo4");
+const TODO5 = document.getElementById("todo5");
+const TODO6 = document.getElementById("todo6");
+const TODO7 = document.getElementById("todo7");
+const TODO8 = document.getElementById("todo8");
+const TODO9 = document.getElementById("todo9");
+const TODO10 = document.getElementById("todo10");
 
-let todo1 = 1000;
-let todo2 = 1000;
-let todo3 = 1000;
+const TODOS = [
+  {
+    name: TODO1,
+    property: { duration: 1, x: -300, y: -300 },
+  },
+  {
+    name: TODO2,
+    property: { duration: 1, x: 300, y: 300 },
+  },
+  {
+    name: TODO3,
+    property: { duration: 1, x: -300, y: 300 },
+  },
+  {
+    name: TODO4,
+    property: { duration: 1, x: -300, y: -300 },
+  },
+  {
+    name: TODO5,
+    property: { duration: 1, x: 300, y: 300 },
+  },
+  {
+    name: TODO6,
+    property: { duration: 1, x: -300, y: 300 },
+  },
+  {
+    name: TODO7,
+    property: { duration: 1, x: -300, y: -300 },
+  },
+  {
+    name: TODO8,
+    property: { duration: 1, x: 300, y: 300 },
+  },
+  {
+    name: TODO9,
+    property: { duration: 1, x: -300, y: 300 },
+  },
+  {
+    name: TODO10,
+    property: { duration: 1, x: -300, y: 300 },
+  },
+];
 
-const stopLeft = () => {
-  clearInterval(toBrightLeft);
-};
+gsap
+  .timeline()
+  .from(".left-image", { scale: 2 })
+  .to(".left-image-wrapper", {
+    clipPath: "polygon(0% 0%,100% 0%,100% 100%,0% 100%)",
+  })
+  .to(".left-image-wrapper", { scale: 0.95, duration: 2 });
 
-let initialPosition = 0;
+setTimeout(() => {
+  FRAME1.style.visibility = "visible";
+}, 2000);
 
-let initialBrightnessLeft = 0;
-let initialBrightnessCenter = 0;
+setTimeout(() => {
+  CENTER_IMAGE.style.visibility = "visible";
+  gsap
+    .timeline()
+    .from(".center-image", { scale: 2 })
+    .to(".center-image-wrapper", {
+      clipPath: "polygon(0% 0%,100% 0%,100% 100%,0% 100%)",
+    })
+    .to(".center-image-wrapper", { scale: 0.95, duration: 2 });
+}, 3000);
 
-const toBrightLeft = setInterval(() => {
-  LEFT_IAMGE.style.visibility = "visible";
-  LEFT_IAMGE.style.filter = `brightness(${initialBrightnessLeft}%)`;
-  initialBrightnessLeft += 6;
-  if (initialBrightnessLeft === 90) {
-    stopLeft();
-    FRAME1.style.visibility = "visible";
-    setTimeout(() => {
-      CENTER_IMAGE.style.visibility = "visible";
-    }, 1000);
-    const stopCenter = () => {
-      clearInterval(toBrightCenter);
-    };
-    const toBrightCenter = setInterval(() => {
-      CENTER_IMAGE.style.filter = `brightness(${initialBrightnessCenter}%)`;
-      initialBrightnessCenter += 6;
-      if (initialBrightnessCenter === 90) {
-        stopCenter();
-        FRAME2.style.visibility = "visible";
-        setTimeout(() => {
-          const stop1 = () => {
-            clearInterval(moveToLeft1);
-          };
+setTimeout(() => {
+  FRAME2.style.visibility = "visible";
+}, 4000);
 
-          const stop2 = () => {
-            clearInterval(moveToLeft2);
-          };
-
-          const stop3 = () => {
-            clearInterval(moveToLeft3);
-          };
-
-          const moveToLeft1 = setInterval(() => {
-            RIGHT_IMAGE1.style.right = initialPosition + "px";
-            RIGHT_IMAGE1.style.top = initialPosition + "px";
-            initialPosition++;
-            todo1--;
-            if (todo1 === 940) {
-              stop1();
-            }
-          }, 10);
-
-          const moveToLeft2 = setInterval(() => {
-            RIGHT_IMAGE2.style.right = initialPosition + "px";
-            initialPosition++;
-            todo2--;
-            if (todo2 === 900) {
-              stop2();
-            }
-          }, 10);
-
-          const moveToLeft3 = setInterval(() => {
-            RIGHT_IMAGE3.style.left = initialPosition + "px";
-            RIGHT_IMAGE3.style.top = initialPosition + "px";
-            initialPosition++;
-            todo3--;
-            if (todo3 === 920) {
-              stop3();
-            }
-          }, 10);
-        }, 1000);
-      }
-    }, 100);
-  }
-}, 100);
+setTimeout(() => {
+  TODOS.forEach((todo) => {
+    todo.name.style.visibility = "visible";
+    gsap.from(todo.name, todo.property);
+  });
+}, 5000);
